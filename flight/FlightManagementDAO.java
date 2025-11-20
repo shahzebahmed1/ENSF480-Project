63a8e210d7b488c92e9b70706424306cd5ad25cf
officialRababRahman <rababrahman04@gmail.com>
2025-11-20 16:27:49 -0700

    add package

--- flight/FlightManagementDAO.java
+++ flight/FlightManagementDAO.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
+import flight.DatabaseConnectivity;
 public class FlightManagementDAO {
 
     private static final String BASE_SELECT =
